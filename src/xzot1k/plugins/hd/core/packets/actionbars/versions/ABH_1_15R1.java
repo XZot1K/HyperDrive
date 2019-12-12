cb7047bd244a5ecd4769c9f8d9904f26b713d85e
BuildTools <unconfigured@null.spigotmc.org>
2019-12-12 01:19:20 -0500

    * Implemented 1.15 packet support and updated the native version.
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

---
+++ src/xzot1k/plugins/hd/core/packets/actionbars/versions/ABH_1_15R1.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019. All rights reserved.
+ */
+
+package xzot1k.plugins.hd.core.packets.actionbars.versions;
+
+import net.minecraft.server.v1_15_R1.IChatBaseComponent;
+import net.minecraft.server.v1_15_R1.PacketPlayOutTitle;
+import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import xzot1k.plugins.hd.core.packets.actionbars.ActionBarHandler;
+
+public class ABH_1_15R1 implements ActionBarHandler {
+    @Override
+    public void sendActionBar(Player player, String message) {
+        PacketPlayOutTitle title = new PacketPlayOutTitle(PacketPlayOutTitle.EnumTitleAction.ACTIONBAR, IChatBaseComponent.ChatSerializer
+                .a("{\"text\": \"" + ChatColor.translateAlternateColorCodes('&', message) + "\"}"));
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(title);
+    }
+
+}
